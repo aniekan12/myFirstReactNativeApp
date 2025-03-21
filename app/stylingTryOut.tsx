36fc243b06b882dd2308b46c1abd442914bc6674
akpakpan aniekan-abasi <45481950+aniekan12@users.noreply.github.com>
2025-03-21 11:15:51 +0100

    Update stylingTryOut.tsx

--- app/stylingTryOut.tsx
+++ app/stylingTryOut.tsx
@@ -38,7 +38,7 @@ export default function StylingTryOut() {
 
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
+    flex: 1,
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "red",
